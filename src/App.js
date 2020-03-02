e3f51e7b16d641f285e3809767a615a1bcfc514f
Andrew Haberman <a.haberman25@gmail.com>
2020-03-02 15:22:56 -0500

    changing function to class and using state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: 'Hello Andrew'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello
+          {this.state.string}
         </p>
         <a
           className="App-link"
@@ -20,7 +28,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
