fde9a362abe7d2fef3362066ddcf00c41a8f4c1d
Andrew Haberman <a.haberman25@gmail.com>
2020-03-10 16:28:04 -0400

    created dynamic search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component'
 
 class App extends Component {
   constructor() {
@@ -18,10 +19,15 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    // const monsters = this.state.monsters
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className="App">
-        <input type='search' placeholder='search monster' onChange={e => {this.setState({searchField: e.target.value})}} />
-        <CardList monsters={this.state.monsters} />       
+        <SearchBox placeholder='search monsters' handlechange={e => {this.setState({searchField: e.target.value})}} />
+        <CardList monsters={filteredMonsters} />       
       </div>
     )
   }
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox = ({ placeholder, handlechange }) => (
+    <input type='search' className='search' placeholder={placeholder} onChange={handlechange} />
+)
